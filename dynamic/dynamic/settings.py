9d4dbb1a397ff491f35e5cee322c08e2e9bf784f
Yathish Acharya <yathishkumar2013@gmail.com>
2024-08-29 16:34:31 +0530

    deploying time

--- dynamic/dynamic/settings.py
+++ dynamic/dynamic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1edi$l*^nu0@=y5vv4)yr-^fyc+!(&k0x)e-7ir*gkyb27x5ve
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
